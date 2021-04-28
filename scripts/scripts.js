687d495585b242401afb95e6a6101d5cfec34145
unknown <jdoucetoc@gmail.com>
2021-04-28 22:14:00 +0200

    Updated best movie section

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,26 @@
+function movie_modal(movie){
+    // code here
+}
+
+function get_best_movie(url){
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let movie = data.results[0];
+            let img_url = movie.image_url;
+            let title = movie.title;
+            let description = movie.description;
+            let img_html = document.createElement("img");
+            img_html.src = img_url
+            img_html.alt = "Best movie image"
+            document.getElementById("best_movie").appendChild(img_html)
+            document.getElementById("best_title").innerHTML = title;
+        })
+        }
+
 const urlmovie = "http://localhost:8000/api/v1/titles/"
 const urlgenre = "http://localhost:8000/api/v1/genres/"
 const urlimdb = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score";
 
-function best_movie(){
-    fetch(urlimdb)
-        .then(response => response.json())
-        .then(data => {
-            movie = data.results[0];
-            img = movie.image_url;
-            var imghtml = document.createElement("img");
-            imghtml.setAttribute("src", img);
-            imghtml.setAttribute("alt", "The best imdb Movie");
-            imghtml.setAttribute("class", "center")
-            document.getElementById("bestmovie").appendChild(imghtml);
-        })
-        }
-best_movie()
+get_best_movie(urlimdb)
+
