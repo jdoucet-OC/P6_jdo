2dbc2a0175b1c1ed2076a5a24f5a1ae92a35ae86
unknown <jdoucetoc@gmail.com>
2021-05-03 23:22:49 +0200

    Hotfix css inside js

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -11,42 +11,35 @@
 function carouselize(carousel) {
 
     var productList = carousel.querySelector('.js-product-list');
-    var productListWidth = 0;
+    var productListWidth = 2205;
     var productListSteps = 0;
     var products = carousel.querySelectorAll('.product-list__item');
-    var productAmount = 0;
+    var productAmount = 7;
     var productAmountVisible = Math.round(window.innerWidth/315);
+    var productMaxStep = productAmount-productAmountVisible;
     var carouselPrev = carousel.querySelector('.js-carousel-prev');
     var carouselNext = carousel.querySelector('.js-carousel-next');
-
-    //window Resize
     window.addEventListener('resize', function(){
         productAmountVisible = Math.round(window.innerWidth/315);
-    });
-
-    //Count all the products
-    [].forEach.call(products, function(product) {
-        productAmount++;
-        productListWidth += 315;
-        productList.style.width = productListWidth+"px";
+        productMaxStep = productAmount-productAmountVisible;
+        if (productListSteps>productMaxStep){
+            productListSteps=productMaxStep;
+        }
     });
 
     carouselNext.onclick = function() {
-    if(productListSteps < productAmount-productAmountVisible) {
         productListSteps++;
-        moveProductList();
-    }
+        if (productListSteps>productMaxStep){
+            productListSteps=productMaxStep;
+        }
+        productList.className="product-list js-product-list step"+productListSteps;
     }
     carouselPrev.onclick = function() {
-    if(productListSteps > 0) {
-        productListSteps--;
-        moveProductList();
-    }
-    }
 
-    // This is a bit hacky, let me know if you find a better way to do this!
-    // Move the carousels product-list
-    function moveProductList() {
-        productList.style.transform = "translateX(-"+315*productListSteps+"px)";
-  }
+        productListSteps--;
+        if (productListSteps<0){
+            productListSteps=0;
+        }
+        productList.className="product-list js-product-list step"+productListSteps;
+    }
 }
